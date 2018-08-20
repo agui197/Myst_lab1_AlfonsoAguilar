4c2c7f41c184c0b05f39d6154f66ccb2068081c8
Alfonso Aguilar <tripleaguilar@hotmail.com>
2018-08-20 08:29:06 -0500

    Actualizacion hasta la funcion

--- Lab1_Myst.R
+++ Lab1_Myst.R
@@ -1,14 +1,35 @@
 # Laboratorio 1
 
+
 #remover objetos del enviroment
-rm(list=list())
+
 
 ###Cargar librerias a utilizar
-supressMessages(library(plotly))  #graficas interactivas
-supressMessages(library(Quandl))  #Descargar precios
-supressMessages(library(PortfolioAnalytics))  #teoria moderna de portafolios
-supressMessages(library(ROI))  #optimizacion para portafolio
-supressMessages(library(knitr))  #opciones de documentacion + codigo
-supressMessages(library(kableExtra))  #tablas en HTML
+(library(plotly))  #graficas interactivas
+(library(Quandl))  #Descargar precios
+(library(PortfolioAnalytics))  #teoria moderna de portafolios
+(library(ROI))  #optimizacion para portafolio
+(library(knitr))  #opciones de documentacion + codigo
+(library(kableExtra))  #tablas en HTML
 
 
+options(knitr.table.format = "html") 
+
+# Cargar el token de QUANDL
+Quandl.api_key("Us_v4rfs-m_kLT1skgsQ")
+
+# Funcion para descagar precios
+Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
+  
+  # Funcion para descargar N cantidad de activos desde QUANDL
+  # -- Dependencias: QUANDL
+  # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
+  # -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA"
+  # -- Fecha_In : Fecha Inicial : character : "2017-01-02"
+  # -- Fecha_Fn : Fecha Final : character : "2017-08-02"
+  
+  # Peticion para descargar precios
+  Datos <- Quandl.datatable(code = "WIKI/PRICES", qopts.columns=Columns, ticker=Tickers,
+                            date.gte=Fecha_In, date.lte=Fecha_Fn)
+  return(Datos)
+}
