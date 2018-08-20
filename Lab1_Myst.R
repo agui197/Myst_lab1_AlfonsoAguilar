08546c22cecb70ab97527d8f052f9ec537c4cd0d
Alfonso Aguilar <tripleaguilar@hotmail.com>
2018-08-20 08:11:18 -0500

    Primer codigo de R de prueba

---
+++ Lab1_Myst.R
@@ -0,0 +1,14 @@
+# Laboratorio 1
+
+#remover objetos del enviroment
+rm(list=list())
+
+###Cargar librerias a utilizar
+supressMessages(library(plotly))  #graficas interactivas
+supressMessages(library(Quandl))  #Descargar precios
+supressMessages(library(PortfolioAnalytics))  #teoria moderna de portafolios
+supressMessages(library(ROI))  #optimizacion para portafolio
+supressMessages(library(knitr))  #opciones de documentacion + codigo
+supressMessages(library(kableExtra))  #tablas en HTML
+
+
